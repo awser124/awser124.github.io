9d2566b80ca5c0b95d1985b491f489fbb6281059
awser <130197201+awser124@users.noreply.github.com>
2023-04-29 20:41:37 +0800

    Add files via upload

---
+++ js/wzwh.js
@@ -0,0 +1,8 @@
+
+window.onload = function(){
+	document.write("系统将在3秒内跳转至指定网站，如长时间未跳转，请<a href='/stop/index.html'>点击此处</a>");
+}
+function Redirect(){
+	location = "stop/index.html";
+}
+setTimeout(Redirect, 3000);
